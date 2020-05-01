c3c6f9dc6b081106c71ec9537af4a5d662ff382b
Akashdeep Jassal <akash.deep.jassal@hotmail.com>
2020-05-02 01:21:29 +0530

    Remove Save The best model only

--- keras_maskrcnn/bin/train.py
+++ keras_maskrcnn/bin/train.py
@@ -88,8 +88,8 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
                 '{backbone}_{dataset_type}_{{epoch:02d}}.h5'.format(backbone=args.backbone, dataset_type=args.dataset_type)
             ),
             verbose=1,
-            save_best_only=True,
-            monitor="mAP",
+            # save_best_only=True,
+            # monitor="mAP",
         )
         checkpoint = RedirectModel(checkpoint, model)
         callbacks.append(checkpoint)
